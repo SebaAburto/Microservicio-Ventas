0d6b38030b4b329b0d83d87ee79a8d5b245b8f27
SebaAburto <seba.aburto@duocuc.cl>
2025-05-28 23:02:21 -0400

    Agregue mas opciones en ProductoController de ventas

--- src/main/java/com/venta/venta/controller/ProductoController.java
+++ src/main/java/com/venta/venta/controller/ProductoController.java
@@ -26,7 +26,43 @@ public class ProductoController {
 
     @PostMapping
     public ResponseEntity<Producto> guardar(@RequestBody Producto producto) {
-        Producto nuevo = productoService.save(producto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(nuevo);
+        Producto productoNuevo = productoService.save(producto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productoNuevo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> buscar(@PathVariable Long id) {
+        try {
+            Producto producto = productoService.findById(id);
+            return ResponseEntity.ok(producto);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> actualizar(@PathVariable Long id, @RequestBody Producto producto) {
+        try {
+            Producto prodExistente = productoService.findById(id);
+            prodExistente.setNombre(producto.getNombre());
+            prodExistente.setDescripcion(producto.getDescripcion());
+            prodExistente.setPrecio(producto.getPrecio());
+
+
+            productoService.save(prodExistente);
+            return ResponseEntity.ok(prodExistente);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        try {
+            productoService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
